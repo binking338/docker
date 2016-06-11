ada874bfd337fe5ce31bc173bf2041076a590875
unknown <>
2016-06-11 22:10:38 +0800

    'remove_TOKEN_autogen'

--- alpine-aria2/shell/run.sh
+++ alpine-aria2/shell/run.sh
@@ -1,7 +1,3 @@
-echo "--> generating token"
-tk=$(date +%s | sha256sum | base64 | head -c 16 ; echo) : ${TOKEN:=$tk}
-echo "--> token: $TOKEN"
-
 aria2c --dir=/downloads --rpc-secret=$TOKEN --enable-rpc=true --rpc-listen-all=true --rpc-allow-origin-all=true
 
 echo
