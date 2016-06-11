9238510645dd243350db1c4e9252b1d2b252d29c
unknown <>
2016-06-11 23:31:07 +0800

    run.sh

--- alpine-shadowsocks/shell/ssserver.sh
+++ alpine-shadowsocks/shell/ssserver.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo "--> server_addr: $SERVER_ADDR"
 echo "--> server_port: $SERVER_PORT"
-tk=$(date +%s | sha256sum | base64 | head -c 16 ; echo) : ${PASSWORD:=$tk}
 echo "--> password: $PASSWORD"
 echo "--> method: $METHOD"
 echo "--> timeout: $TIMEOUT"
