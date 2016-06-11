2b60fea6666ad1a9dc8df763241f93068886cc53
unknown <>
2016-06-11 23:19:29 +0800

    run.sh

--- alpine-aria2/shell/run.sh
+++
@@ -1,5 +0,0 @@
-echo "--> generating token"
-tk=$(date +%s | sha256sum | base64 | head -c 16 ; echo) : ${TOKEN:=$tk}
-echo "--> token: $TOKEN"
-touch /etc/arai2/aria2.session
-aria2c --conf-path=/etc/aria2/aria2.conf
