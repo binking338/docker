e69901032591f4d7138c910f24ac3ebaaef03af7
王斌 <wangbin@wangbindeMacBook-Pro.local>
2016-06-12 11:02:05 +0800

    mv-

--- alpine-aria2/shell/run.sh
+++ alpine-aria2/shell/run.sh
@@ -3,5 +3,5 @@
 #[ -z $RPC_SECRET ] && RPC_SECRET=$(date +%s | sha256sum | base64 | head -c 16 ; echo)
 #echo "--> rpc_secret: $RPC_SECRET"
 
-[ ! -r /etc/aria2/aria2.conf ] && mv /aria2.conf /etc/aria2/aria2.conf
+[ ! -r /etc/aria2/aria2.conf ] && cp /aria2.conf /etc/aria2/aria2.conf
 aria2c --conf-path=/etc/aria2/aria2.conf
